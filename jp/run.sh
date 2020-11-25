f41029444c7342578e8eeed09814645430f5bfd8
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-11-25 07:48:59 +0000

    fix

--- jp/run.sh
+++ jp/run.sh
@@ -5,5 +5,5 @@ ICM_REPO=$(cat ../ICM/ICMDurable/CONF_ICM_REPO)
 
 printf "\nStarting ICM with $ICM_REPO:$ICM_TAG..."
 docker run --rm -v $PWD/../ICM/ICMDurable:/ICMDurable -v $PWD:/shells --cap-add SYS_TIME $ICM_REPO:$ICM_TAG /shells/exec.sh
-#docker run --rm -it -v $PWD/../ICM/ICMDurable:/ICMDurable --cap-add SYS_TIME $ICM_REPO:$ICM_TAG < exec.sh
+#docker run --rm -it -v $PWD/../ICM/ICMDurable:/ICMDurable --cap-add SYS_TIME $ICM_REPO:$ICM_TAG 
 printf "\nExited icm container\n"
