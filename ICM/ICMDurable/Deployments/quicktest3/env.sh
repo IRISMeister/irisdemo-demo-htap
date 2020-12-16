14355d02ebc871070e0eec9f4a88cc7aacc3332c
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-12-16 10:24:26 +0900

    container mirrored

---
+++ ICM/ICMDurable/Deployments/quicktest3/env.sh
@@ -0,0 +1,14 @@
+export DEPLOYMENT_FOLDER=/ICMDurable/Deployments/quicktest3
+export SSH_DIR=/ICMDurable/keys
+export TLS_DIR=/ICMDurable/keys
+export ICM_LABEL=quicktest3
+export IRIS_HOSTNAME=iris-quicktest3-DM-IRISSpeedTest-0001.weave.local
+export IRIS_ECP_HOSTNAME=iris-quicktest3-DM-IRISSpeedTest-0001.weave.local
+export HTAP_MASTERS=1
+export HTAP_INGESTION_WORKERS=1
+export HTAP_QUERY_WORKERS=1
+export MAX_CN=3
+export DOCKER_USERNAME=xxx
+export DOCKER_PASSWORD=yyy
+export CONTAINERLESS=false
+export NR_HUGE_PAGES=
